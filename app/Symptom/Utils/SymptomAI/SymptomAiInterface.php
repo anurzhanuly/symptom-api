71ed6991c98f37eb893ae9045c884aa92c7a8ef1
anurzhanuly <isaxved@gmail.com>
2023-03-16 20:30:17 +0600

    an-2 прокидывает язык для перевода

--- app/Symptom/Utils/SymptomAI/SymptomAiInterface.php
+++ app/Symptom/Utils/SymptomAI/SymptomAiInterface.php
@@ -8,8 +8,9 @@ interface SymptomAiInterface
      * Метод для получения рекомендации от МЛ модели Symptom-ai.
      * Который даёт рекомендации в зависимости от ответов пользователя на опросник
      *
-     * @param array $questionnaireResponse
+     * @param  array  $questionnaireResponse
+     * @param  string $lang
      * @return mixed
      */
-    public function getRecommendations(array $questionnaireResponse);
+    public function getRecommendations(array $questionnaireResponse, string $lang = 'ru');
 }
