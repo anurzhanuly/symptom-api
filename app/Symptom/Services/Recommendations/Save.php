aec8b999d053d1a36059dd6fdc9320262f301956
anurzhanuly <isaxved@gmail.com>
2023-06-17 01:22:59 +0600

    an-15-fix-recommendations feat устанавливает ИМТ пользователя

--- app/Symptom/Services/Recommendations/Save.php
+++ app/Symptom/Services/Recommendations/Save.php
@@ -9,6 +9,10 @@ use App\Symptom\Services\Commands\ResultsSaveCommand;
 
 class Save
 {
+    public const HEIGHT_QUESTION_NAME = 'Рост:';
+
+    public const WEIGHT_QUESTION_NAME = 'Вес:';
+
     protected ResultRepository $resultsRepository;
 
     public function __construct(ResultRepository $resultsRepository)
@@ -24,12 +28,30 @@ class Save
             $result->setDoctorID($command->getDoctorID());
         }
 
+        $userAnswers         = $command->getPatientAnswers();
+        $bmi                 = $this->getUserBMI($userAnswers);
+        $userAnswers['ИМТ:'] = $bmi;
+
         $result->setPatientID($command->getPatientID())
             ->setRecommendations($command->getRecommendations())
             ->setSymptomAI($command->getSymptomAIRecommendations())
             ->setPatientCard($command->getPatientCard())
-            ->setPatientAnswers($command->getPatientAnswers());
+            ->setPatientAnswers($userAnswers);
 
         $result->save();
+    }
+
+    private function getUserBMI(array $userAnswers): float
+    {
+        $height = $userAnswers[self::HEIGHT_QUESTION_NAME] ?? 0;
+        $weight = $userAnswers[self::WEIGHT_QUESTION_NAME] ?? 0;
+
+        if (empty($height) || empty($weight)) {
+            return 0;
+        }
+
+        $height = $height / 100;
+
+        return $weight / pow($height, 2);
     }
 }
