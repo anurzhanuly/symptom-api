a90dca6da3feab2b65ec2ce6689bd1919a1c1cf8
anurzhanuly <isaxved@gmail.com>
2023-06-21 20:15:41 +0600

    an-16-reset-password feat Допиливает контроллер

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Symptom\Services\Commands\UserUpdateCommand;
+use App\Symptom\Services\Users\Update;
+use App\Symptom\Transformers\User;
+use Illuminate\Http\Client\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class UsersController extends Controller
 {
-    public function update(): JsonResponse
+    public function update(Request $request, Update $userUpdateService, User $userTransformer): JsonResponse
     {
-        return $this->sendResponse([], 'User updated successfully.');
+        return response()->json(
+            $this->item(
+                $userUpdateService->execute(
+                    new UserUpdateCommand(
+                        $request->get('user')->getId(),
+                        $request->get('password')
+                    )
+                ),
+                $userTransformer
+            )
+        );
     }
 }
