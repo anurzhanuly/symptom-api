ed35b9bfbc246dab062fe09a4867586b9aaf71f3
anurzhanuly <isaxved@gmail.com>
2023-07-12 02:14:18 +0600

    an-19-fix-recommendations тест на выдачу логику проверки рекомендации

--- tests/Unit/services/recommendations/Test.php
+++ tests/Unit/services/recommendations/Test.php
@@ -15,22 +15,68 @@ class Test extends TestCase
      * @return void
      * @throws \ReflectionException
      */
-    public function testConditionApplies()
+    public function testConditionApplies(array $condition, array $userAnswer, bool $expected)
     {
         $mockRepository = $this->createMock(RecommendationsRepository::class);
         $recommendationsService = new GetRecommendations($mockRepository);
         $method = (new \ReflectionClass(GetRecommendations::class))->getMethod(self::METHOD_NAME);
-        $result = $method->invoke($recommendationsService, ['operator' => '==', 'value' => 'yes'], 'yes');
+        $result = $method->invoke($recommendationsService, $condition, $userAnswer);
 
-        $this->assertTrue($result);
+        $this->assertEquals($expected, $result);
     }
 
     public function getDataTestConditionApplies(): array
     {
         return [
-            [
-                'condition' => ['compare' => 'except', 'value' => 'yes'],
-                'userAnswer' => 'yes',
+            'False except condition' => [
+                'condition' => ['compare' => 'except', 'value' => ['yes']],
+                'userAnswer' => ['yes'],
+                'expectedResult' => false,
+            ],
+            'True except condition' => [
+                'condition' => ['compare' => 'except', 'value' => ['yes']],
+                'userAnswer' => ['no'],
+                'expectedResult' => true,
+            ],
+            'False exact condition' => [
+                'condition' => ['compare' => 'exact', 'value' => ['yes']],
+                'userAnswer' => ['no'],
+                'expectedResult' => false,
+            ],
+            'True exact condition' => [
+                'condition' => ['compare' => 'exact', 'value' => ['yes']],
+                'userAnswer' => ['yes'],
+                'expectedResult' => true,
+            ],
+            'False range condition' => [
+                'condition' => ['compare' => 'range', 'value' => ['1', '2']],
+                'userAnswer' => ['3'],
+                'expectedResult' => false,
+            ],
+            'True range condition' => [
+                'condition' => ['compare' => 'range', 'value' => ['1', '2']],
+                'userAnswer' => ['1'],
+                'expectedResult' => true,
+            ],
+            'False less condition' => [
+                'condition' => ['compare' => 'less', 'value' => ['1']],
+                'userAnswer' => ['2'],
+                'expectedResult' => false,
+            ],
+            'True less condition' => [
+                'condition' => ['compare' => 'less', 'value' => ['1']],
+                'userAnswer' => ['0'],
+                'expectedResult' => true,
+            ],
+            'False greater condition' => [
+                'condition' => ['compare' => 'greater', 'value' => ['1']],
+                'userAnswer' => ['0'],
+                'expectedResult' => false,
+            ],
+            'True greater condition' => [
+                'condition' => ['compare' => 'greater', 'value' => ['1']],
+                'userAnswer' => ['2'],
+                'expectedResult' => true,
             ],
         ];
     }
